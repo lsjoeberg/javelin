fe583bf3a8eb1ec2dff4cbcc4b68957d218235b2
Lars Sjöberg <lsjoeberg@protonmail.com>
2023-10-01 22:08:18 +0200

    Add MWE of flight server with DoGet on parquet

---
+++ src/main.rs
@@ -0,0 +1,119 @@
+use futures::stream::BoxStream;
+use std::fs::File;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use tonic::transport::Server;
+use tonic::{Request, Response, Status, Streaming};
+
+use arrow_flight::encode::FlightDataEncoderBuilder;
+use arrow_flight::error::FlightError;
+use arrow_flight::{
+    flight_service_server::FlightService, flight_service_server::FlightServiceServer, Action,
+    ActionType, Criteria, Empty, FlightData, FlightDescriptor, FlightInfo, HandshakeRequest,
+    HandshakeResponse, PutResult, SchemaResult, Ticket,
+};
+use futures::TryStreamExt;
+use parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder;
+
+#[derive(Clone)]
+pub struct FlightServiceImpl {}
+
+#[tonic::async_trait]
+impl FlightService for FlightServiceImpl {
+    type HandshakeStream = BoxStream<'static, Result<HandshakeResponse, Status>>;
+    async fn handshake(
+        &self,
+        _request: Request<Streaming<HandshakeRequest>>,
+    ) -> Result<Response<Self::HandshakeStream>, Status> {
+        Err(Status::unimplemented("Implement handshake"))
+    }
+    type ListFlightsStream = BoxStream<'static, Result<FlightInfo, Status>>;
+    async fn list_flights(
+        &self,
+        _request: Request<Criteria>,
+    ) -> Result<Response<Self::ListFlightsStream>, Status> {
+        Err(Status::unimplemented("Implement list_flights"))
+    }
+    async fn get_flight_info(
+        &self,
+        _request: Request<FlightDescriptor>,
+    ) -> Result<Response<FlightInfo>, Status> {
+        Err(Status::unimplemented("Implement get_flight_info"))
+    }
+    async fn get_schema(
+        &self,
+        _request: Request<FlightDescriptor>,
+    ) -> Result<Response<SchemaResult>, Status> {
+        Err(Status::unimplemented("Implement get_schema"))
+    }
+    type DoGetStream = BoxStream<'static, Result<FlightData, Status>>;
+
+    async fn do_get(
+        &self,
+        _request: Request<Ticket>,
+    ) -> Result<Response<Self::DoGetStream>, Status> {
+        // TODO: Parse ticket...
+
+        let file = File::open("data/integers.parquet").unwrap();
+
+        let builder = ParquetRecordBatchReaderBuilder::try_new(file).unwrap();
+        println!("Converted arrow schema is: {}", builder.schema());
+
+        let reader = builder.build().unwrap();
+        let batches = reader.map(|res| res.map_err(FlightError::from));
+
+        let input_stream = futures::stream::iter(batches);
+        let flight_data_stream = FlightDataEncoderBuilder::new().build(input_stream);
+        Ok(Response::new(Box::pin(
+            flight_data_stream.map_err(Status::from),
+        )))
+    }
+
+    type DoPutStream = BoxStream<'static, Result<PutResult, Status>>;
+
+    async fn do_put(
+        &self,
+        _request: Request<Streaming<FlightData>>,
+    ) -> Result<Response<Self::DoPutStream>, Status> {
+        Err(Status::unimplemented("Implement do_put"))
+    }
+
+    type DoExchangeStream = BoxStream<'static, Result<FlightData, Status>>;
+
+    async fn do_exchange(
+        &self,
+        _request: Request<Streaming<FlightData>>,
+    ) -> Result<Response<Self::DoExchangeStream>, Status> {
+        Err(Status::unimplemented("Implement do_exchange"))
+    }
+
+    type DoActionStream = BoxStream<'static, Result<arrow_flight::Result, Status>>;
+
+    async fn do_action(
+        &self,
+        _request: Request<Action>,
+    ) -> Result<Response<Self::DoActionStream>, Status> {
+        Err(Status::unimplemented("Implement do_action"))
+    }
+
+    type ListActionsStream = BoxStream<'static, Result<ActionType, Status>>;
+
+    async fn list_actions(
+        &self,
+        _request: Request<Empty>,
+    ) -> Result<Response<Self::ListActionsStream>, Status> {
+        Err(Status::unimplemented("Implement list_actions"))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 50051);
+    let service = FlightServiceImpl {};
+
+    let svc = FlightServiceServer::new(service);
+
+    println!("Starting service on {}", addr);
+    Server::builder().add_service(svc).serve(addr).await?;
+
+    Ok(())
+}
